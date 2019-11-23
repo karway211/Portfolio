9342a03961f07593512d5dbdd1f15cf5763171df
Andrey K <karway211@gmail.com>
2019-11-23 17:53:11 +0300

    fix: fix slider

--- index.js
+++ index.js
@@ -1,130 +1,166 @@
-const slider = document.getElementById('slider'),
-    carousel = document.getElementById('carousel'),
-    leftArrow = document.getElementById('left-arrow'),
-    rightArrow = document.getElementById('right-arrow'),
-    wrap = document.getElementById('projects-wrapper');
-
-const slide = (items, leftArrow, rightArrow) => {
-    let x1 = 0,
-        x2 = 0,
-        start = items.offsetLeft,
-        finish,
-        moveMouse = 40,
-        slides = items.getElementsByClassName('slide'),
-        slidesLength = slides.length,
-        slideSize = items.getElementsByClassName('slide')[0].offsetWidth,
-        firstSlide = slides[0],
-        lastSlide = slides[slidesLength - 1],
-        cloneFirstSlide = firstSlide.cloneNode(true),
-        cloneLastSlide = lastSlide.cloneNode(true),
-        allowChange = true,
-        index = 0;
-        items.appendChild(cloneFirstSlide);
-        items.insertBefore(cloneLastSlide, firstSlide);
-        const discription = document.querySelectorAll('.slide-elem');
-        const buttonShow = document.getElementById('buttonShow');
-
-    window.addEventListener('resize', () => {
-        slideSize = items.getElementsByClassName('slide')[0].offsetWidth;
-        items.style.left = -slideSize + 'px';
-        if(window.innerWidth <= 600) {
-            discription.forEach((elem) => {
-                elem.style.display = 'none';
-            });
-        } else {
-            discription.forEach((elem) => {
-                elem.style.display = 'block';
-            });
-        }
-    });
-    
-    const dragStart = (e) => {
-        start = items.offsetLeft;
-        if (e.type == 'touchstart') {
-            x1 = e.touches[0].clientX;
-        }
-    }
-    
-    items.addEventListener('touchstart', dragStart);
-
-    const dragAction = (e) => {
-        if (e.type == 'touchmove') {
-            x2 = x1 - e.touches[0].clientX;
-            x1 = e.touches[0].clientX;
-        } 
-        items.style.left = (items.offsetLeft - x2) + "px";
-    }
-
-    items.addEventListener('touchmove', dragAction);
-    
-    const changeSlides = (dir, action) => {
-        items.classList.add('changing');
-        if (allowChange) {
-            if (!action) {
-                start = items.offsetLeft;
-            }
-            if (dir == 1) {
-                items.style.left = (start - slideSize) + "px";
-                index++;
-            } else if (dir == -1) {
-                items.style.left = (start + slideSize) + "px";
-                index--;
-            }
-        }
-        allowChange = false;
-    }
-    
-    leftArrow.addEventListener('click', () => { changeSlides(-1) });
-    rightArrow.addEventListener('click', () => { changeSlides(1) });
-
-    const dragFinish = () => {
-        finish = items.offsetLeft;
-        if (finish - start < -moveMouse) {
-            changeSlides(1, 'drag');
-        } else if (finish - start > moveMouse) {
-            changeSlides(-1, 'drag');
-        }
-    };
-  
-    items.addEventListener('touchend', dragFinish);
-    
-    const checkIndex = () => {
-        items.classList.remove('changing');
-        if (index == -1) {
-            items.style.left = -(slidesLength * slideSize) + "px";
-            index = slidesLength - 1;
-        }
-        if (index == slidesLength) {
-            items.style.left = -(1 * slideSize) + "px";
-            index = 0;
-        }
-        allowChange = true;
-    };
-
-    items.addEventListener('transitionend', checkIndex);
-    buttonShow.onclick = (e) => {
-        if (e.target.value === 'show discription') {
-            e.target.value = 'out discription';
-            discription.forEach((elem) => {
-                elem.style.display = 'block';
-            });
-        } else {
-            e.target.value = 'show discription';
-            discription.forEach((elem) => {
-                elem.style.display = 'none';
-            });
-        }
-    }
-}
-
-slide(carousel, leftArrow, rightArrow);
-
 const menuElem = document.getElementById('edukation');
 const titleElem = menuElem.querySelector('.edukation__click');
+const discriptionName = document.querySelectorAll('.project-name');
 
 titleElem.onclick = () => {
-menuElem.classList.toggle('open');
+    menuElem.classList.toggle('open');
 };
+
+const buttonShow = document.getElementById('buttonShow');
+buttonShow.addEventListener('click', (e) => {
+    console.log(e);
+    if (e.target.value === 'show discription') {
+        e.target.value = 'out discription';
+        discriptionName.forEach((elem) => {
+            elem.style.display = 'block';
+        });
+    } else {
+        e.target.value = 'show discription';
+        discriptionName.forEach((elem) => {
+            elem.style.display = 'none';
+        });
+    }
+})
+
+let items = document.querySelectorAll('.carousel .item');
+let el = document.querySelector('.carousel');
+let currentItem = 0;
+let isEnabled = true;
+
+function changeCurrentItem(n) {
+	currentItem = (n + items.length) % items.length;
+}
+
+function hideItem(direction) {
+	isEnabled = false;
+	items[currentItem].classList.add(direction);
+	items[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('active', direction);
+	});
+}
+
+function showItem(direction) {
+	items[currentItem].classList.add('next', direction);
+	items[currentItem].addEventListener('animationend', function() {
+		this.classList.remove('next', direction);
+		this.classList.add('active');
+		isEnabled = true;
+	});
+}
+this
+function nextItem(n) {
+	hideItem('to-left');
+	changeCurrentItem(n + 1);
+	showItem('from-right');
+}
+
+function previousItem(n) {
+	hideItem('to-right');
+	changeCurrentItem(n - 1);
+	showItem('from-left');
+}
+
+document.querySelector('.control.left').addEventListener('click', function() {
+	if (isEnabled) {
+		previousItem(currentItem);
+	}
+});
+
+document.querySelector('.control.right').addEventListener('click', function() {
+	if (isEnabled) {
+		nextItem(currentItem);
+	}
+});
+
+const swipedetect = (el) => {
+  
+	let surface = el;
+	let startX = 0;
+	let startY = 0;
+	let distX = 0;
+	let distY = 0;
+	let startTime = 0;
+	let elapsedTime = 0;
+
+	let threshold = 150;
+	let restraint = 100;
+	let allowedTime = 300;
+
+	surface.addEventListener('mousedown', function(e){
+		startX = e.pageX;
+		startY = e.pageY;
+		startTime = new Date().getTime();
+		e.preventDefault();
+	}, false);
+
+	surface.addEventListener('mouseup', function(e){
+		distX = e.pageX - startX;
+		distY = e.pageY - startY;
+		elapsedTime = new Date().getTime() - startTime;
+		if (elapsedTime <= allowedTime){
+			if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
+				if ((distX > 0)) {
+					if (isEnabled) {
+						previousItem(currentItem);
+					}
+				} else {
+					if (isEnabled) {
+						nextItem(currentItem);
+					}
+				}
+			}
+		}
+		e.preventDefault();
+	}, false);
+
+	surface.addEventListener('touchstart', function(e){
+		if (e.target.classList.contains('arrow') || e.target.classList.contains('control')) {
+			if (e.target.classList.contains('left')) {
+				if (isEnabled) {
+					previousItem(currentItem);
+				}
+			} else {
+				if (isEnabled) {
+					nextItem(currentItem);
+				}
+			}
+		}
+			var touchobj = e.changedTouches[0];
+			startX = touchobj.pageX;
+			startY = touchobj.pageY;
+			startTime = new Date().getTime();
+			e.preventDefault();
+	}, false);
+
+	surface.addEventListener('touchmove', function(e){
+			e.preventDefault();
+	}, false);
+
+	surface.addEventListener('touchend', function(e){
+			var touchobj = e.changedTouches[0];
+			distX = touchobj.pageX - startX;
+			distY = touchobj.pageY - startY;
+			elapsedTime = new Date().getTime() - startTime;
+			if (elapsedTime <= allowedTime){
+					if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){
+							if ((distX > 0)) {
+								if (isEnabled) {
+									previousItem(currentItem);
+								}
+							} else {
+								if (isEnabled) {
+									nextItem(currentItem);
+								}
+							}
+					}
+			}
+			e.preventDefault();
+	}, false);
+}
+
+swipedetect(el);
+
+
 
 
 
