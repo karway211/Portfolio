0d61e85300fbfd62c6a16a32a37c3d946e9ed9ed
Andrey Karcheuski <karway211@gmail.com>
2020-06-07 21:50:29 +0300

    feat: add project

---
+++ projects/songbird/precache-manifest.87b53a0be143dc4e0d934f8afb10d387.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa233699164ee4e1af4b1f85c7e86c70",
+    "url": "/songbird/index.html"
+  },
+  {
+    "revision": "5d692996ba909f5933f3",
+    "url": "/songbird/static/css/main.af72c080.chunk.css"
+  },
+  {
+    "revision": "90ac903667b12def02b7",
+    "url": "/songbird/static/js/2.b6959274.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/songbird/static/js/2.b6959274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d692996ba909f5933f3",
+    "url": "/songbird/static/js/main.e7f9497f.chunk.js"
+  },
+  {
+    "revision": "d9d571406ff96f214aa4",
+    "url": "/songbird/static/js/runtime-main.d7308ec5.js"
+  }
+]);
